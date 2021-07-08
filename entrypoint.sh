4e9a1f29902a43a8a1030c7a68053f59189f786d
Tiago Albineli Motta <timotta@gmail.com>
2021-07-08 15:18:59 -0300

    Fix wait file position on path

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ function deploy() {
 function wait() {
     echo Waiting $IMAGE to finish deploy in $URL...
     pip install requests==2.25.1 && \
-        python wait.py $IMAGE $URL $TIMEOUT
+        python /wait.py $IMAGE $URL $TIMEOUT
 }
 
 deploy && wait
